cdf7aef58e9f53904b74b3da334dc314002e71e3
tlfqk13 <fkffksk20@gmail.com>
2022-06-14 21:22:35 +0900

    Initial commit

---
+++ src/main/java/com/example/awshomework/AwsHomeworkApplication.java
@@ -0,0 +1,14 @@
+package com.example.awshomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsHomeworkApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AwsHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awshomework/web/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.awshomework.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void helloReturn() throws Exception{
+        String hello = "hello";
+
+        this.mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+}
