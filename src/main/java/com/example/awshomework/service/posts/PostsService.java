978f002dc8c42f5bbb7fb06b81c8a1d9831e2bdf
tlfqk13 <fkffksk20@gmail.com>
2022-06-15 12:19:13 +0900

    chapter 4 장까지 마무리

--- src/main/java/com/example/awshomework/service/posts/PostsService.java
+++ src/main/java/com/example/awshomework/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.awshomework.service.posts;
 
 import com.example.awshomework.domain.posts.PostRepository;
 import com.example.awshomework.domain.posts.Posts;
+import com.example.awshomework.web.dto.PostsListResponseDto;
 import com.example.awshomework.web.dto.PostsResponseDto;
 import com.example.awshomework.web.dto.PostsSaveRequestDto;
 import com.example.awshomework.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,20 @@ public class PostsService {
         Posts entity = postRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postRepository.findAllDesc()
+                .stream()
+                .map(PostsListResponseDto::new) // == .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+        // postsResponseDto
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id= "+id));
+        postRepository.delete(posts);
     }
 }
---
+++ src/main/java/com/example/awshomework/web/IndexController.java
@@ -0,0 +1,38 @@
+package com.example.awshomework.web;
+
+import com.example.awshomework.service.posts.PostsService;
+import com.example.awshomework.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        // model
+        // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
+        // posts 라는 이름으로 index.mustache 에 전달
+        model.addAttribute("posts",postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+
+        return "posts-update";
+    }
+}
